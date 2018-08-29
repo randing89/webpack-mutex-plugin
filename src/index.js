4ba7fca89d63ef58f6990de5dd7d347ffe510442
Ran Ding <randing89@gmail.com>
2018-08-29 00:55:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const lockfile = require("proper-lockfile");
+
+function withOptions(func) {
+  return (...args) => {
+    return func(...args, {
+      realpath: false
+    });
+  };
+}
+
+const checkFile = withOptions(lockfile.check);
+const lockFile = withOptions(lockfile.lock);
+
+module.exports = class WebpackMutexPlugin {
+  constructor({ file, onLocked } = {}) {
+    if (!file) {
+      throw new Error(`WebpackMutexPlugin requires a lock file path`);
+    }
+
+    this.file = file;
+    this.onLocked = onLocked || (() => null);
+  }
+
+  lock() {
+    checkFile(this.file).then(locked => {
+      if (locked) {
+        return this.onLocked();
+      }
+
+      lockFile(this.file);
+    });
+  }
+
+  apply(compiler) {
+    compiler.hooks.run.tap(WebpackMutexPlugin.name, this.lock.bind(this));
+    compiler.hooks.watchRun.tap(WebpackMutexPlugin.name, this.lock.bind(this));
+
+    // Lock file will be removed automatically after the process exited.
+    process
+      .once("SIGINT", () => process.exit(1))
+      .once("SIGTERM", () => process.exit(1));
+  }
+};
